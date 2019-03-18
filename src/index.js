3e8cc0d639aafe8a07e36bab58515813a8a28e71
Margarita21 <margosha2610@mail.ru>
2019-03-19 02:11:36 +0300

    add 4 task with little mistakes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function getZerosCount(number, base) {
-    	var  k, kol = 0, kol_null = 0, f = 0, main=0;
+    var  k, kol = 0, kol_null = 0, f = 0, main=0;
 	var d_base = base;
 	for( var i = 2; i< base; i++){
 		k = 0;
@@ -17,11 +17,15 @@ module.exports = function getZerosCount(number, base) {
 			main = i;
 		}
 	}
+	if (main === 0)
+	{
+		kol = 1;
+		main = base;
+	}
 	var s=0, st=1;
 	while( (number / Math.pow(main,st)) >= 1 ){
 		s = s + Math.floor(number / Math.pow(main,st));
 		st = st + 1;
-		
 	}
 	return s;
 }
