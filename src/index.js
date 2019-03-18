6c3bb8c97da29b79d7860956118de0aca2acca5e
Margarita21 <margosha2610@mail.ru>
2019-03-19 01:36:04 +0300

    add new task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    	var  k, kol = 0, kol_null = 0, f = 0, main=0;
+	var d_base = base;
+	for( var i = 2; i< base; i++){
+		k = 0;
+		while( d_base % i === 0){
+			k = k + 1;
+			d_base = d_base / i;
+		} 
+		if (k > 0 && f === 0){
+			kol = k;
+			main = i;
+			f = 1;
+		}
+		if (k <= kol && k > 0){
+			kol = k;
+			main = i;
+		}
+	}
+	var s=0, st=1;
+	while( (number / Math.pow(main,st)) >= 1 ){
+		s = s + Math.floor(number / Math.pow(main,st));
+		st = st + 1;
+		
+	}
+	return s;
 }
